6f26d8e26de3eb9b34b7b77b52e2f33399312f02
loki2k <jeremylagarto@gmail.com>
2020-12-13 10:39:53 -0800

    Updates

---
+++ precache-manifest.6a8537c405a14185770683b1ef346d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6516bf6cc1ea9d95a0903cb49fb4ce7f",
+    "url": "/grocery-bud/index.html"
+  },
+  {
+    "revision": "b8e47fa5fd8f846c53ee",
+    "url": "/grocery-bud/static/css/main.a903c182.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-bud/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-bud/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e47fa5fd8f846c53ee",
+    "url": "/grocery-bud/static/js/main.391d88d7.chunk.js"
+  },
+  {
+    "revision": "f19ce6e521be50b493db",
+    "url": "/grocery-bud/static/js/runtime-main.8b040e9c.js"
+  }
+]);
